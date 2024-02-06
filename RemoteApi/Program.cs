aa6843feb9acb32e947020f526c5675fec56febe
Lennartajansson <lennart.a.jansson@outlook.com>
2024-02-06 22:45:21 +0100

    Fixed swagger in Production

--- RemoteApi/Program.cs
+++ RemoteApi/Program.cs
@@ -25,9 +25,9 @@ WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  _ = app.UseSwagger();
-  _ = app.UseSwaggerUI();
 }
+_ = app.UseSwagger();
+_ = app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
